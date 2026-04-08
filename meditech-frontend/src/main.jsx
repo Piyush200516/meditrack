07495208fc530b4713304c8199599fa32b8c1c22
piyush200516 <piyushmishra21052003@gmail.com>
2026-04-08 17:43:00 +0530

    Start-Project

---
+++ meditech-frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1 className="text-3xl font-bold text-blue-600">
+  MediTech Dashboard
+</h1>
+  </StrictMode>,
+)
