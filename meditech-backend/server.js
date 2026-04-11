7f4e1a8dce4b82a488ffecb4a77ac1b25a4ab059
piyush200516 <piyushmishra21052003@gmail.com>
2026-04-11 13:44:27 +0530

    first-commit

---
+++ meditech-backend/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+/* ✅ TEST API */
+app.get("/", (req, res) => {
+  res.send("Server Running ✅");
+});
+
+/* ✅ DB TEST API */
+app.get("/test-db", (req, res) => {
+  db.query("SELECT 1", (err, result) => {
+    if (err) {
+      return res.send("❌ DB Not Connected");
+    }
+    res.send("✅ DB Connected Successfully");
+  });
+});
+
+/* SERVER START */
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT}`);
+});
